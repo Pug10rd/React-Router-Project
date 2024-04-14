d560c6a6aa5f10582ba80b968ac7cdadab0ce99e
Ivan Zarichnyi <91795849+Pug10rd@users.noreply.github.com>
2024-04-14 22:02:46 +0200

    fix 1.4

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter basename="/React-Router-Project">
+  <BrowserRouter basename="/React-Router-Project">
+    <React.StrictMode>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    </React.StrictMode>
+  </BrowserRouter>
 );
